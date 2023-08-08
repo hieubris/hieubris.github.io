f18ba4c338d0f413b3ac6e3efbfc9a2790399428
hieubris <ale19@uci.edu>
2023-08-07 23:56:51 -0700

    testing

--- api.js
+++ api.js
@@ -1,16 +1,8 @@
+const URL = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
+
 var request = new XMLHttpRequest();
-
-request.open('GET', 'https://datausa.io/api/data?drilldowns=Nation&measures=Population', true);
-
-request.onload = function() {
-    var data = JSON.parse(this.response);
-
-    if (request.status >= 200 && request.status < 400) {
-        console.log(data);
-    }
-    else {
-        console.log('error');
-    }
-}
-
+request.open('GET', URL);
 request.send();
+request.onload = ()=> {
+    console.log(JSON.parse(request.reponse));
+}
